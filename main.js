8848e19d26c9943332e0254ea69c9f5a34c1659f
Katy St Sauveur <katyrogowski@yahoo.com>
2020-04-29 07:58:28 -0700

    Remove all info from page on wallet click

--- main.js
+++ main.js
@@ -7,9 +7,14 @@ var dashboardIcon = document.querySelector('.dashboard-icon')
 var dashboardHighlight = document.querySelector('.dashboard-highlight')
 var walletHighlight = document.querySelector('.wallet-highlight')
 var dashboardUnhighlight = document.querySelector('.dashboard-unhighlight')
+var accountHeader = document.querySelector('.title-accounts')
+var specificBankInfo = document.querySelector('.specific-bank-info')
+var transactionInfo = document.querySelector('.column2of2')
+var summaryBoxes = document.querySelector('.box-container')
 
 xClose.addEventListener('click', closeBanner)
 walletIcon.addEventListener('click', toggleHighlight)
+walletIcon.addEventListener('click', clearPage)
 // walletIcon.addEventListener('click', deleteHighlight)
 
 function closeBanner() {
@@ -20,7 +25,15 @@ welcomeText.classList.add('hidden');
 }
 
 function toggleHighlight() {
-
 dashboardIcon.classList.remove('dashboard-highlight');
 walletIcon.classList.add('wallet-highlight')
+}
+
+function clearPage() {
+accountHeader.classList.add('hidden')
+specificBankInfo.classList.add('hidden')
+transactionInfo.classList.add('hidden')
+summaryBoxes.classList.add('hidden')
+
+closeBanner()
 }
