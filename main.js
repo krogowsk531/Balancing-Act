59c3414f60d594faaac7dc99223a94c8ea81db5e
Katy St Sauveur <katyrogowski@yahoo.com>
2020-04-28 19:15:28 -0700

    Toggle highlight between wallet and dashboard

--- main.js
+++ main.js
@@ -2,12 +2,25 @@ var xClose = document.querySelector('.close')
 var welcomeBox = document.querySelector('.welcome-box')
 var boxShaddow = document.querySelector('.box-shadow')
 var welcomeText = document.querySelector('.welcome-text')
+var walletIcon = document.querySelector('.wallet-icon')
+var dashboardIcon = document.querySelector('.dashboard-icon')
+var dashboardHighlight = document.querySelector('.dashboard-highlight')
+var walletHighlight = document.querySelector('.wallet-highlight')
+var dashboardUnhighlight = document.querySelector('.dashboard-unhighlight')
 
 xClose.addEventListener('click', closeBanner)
+walletIcon.addEventListener('click', toggleHighlight)
+// walletIcon.addEventListener('click', deleteHighlight)
 
 function closeBanner() {
 xClose.classList.add('hidden');
 welcomeBox.classList.add('hidden');
 boxShaddow.classList.add('hidden');
 welcomeText.classList.add('hidden');
+}
+
+function toggleHighlight() {
+
+dashboardIcon.classList.remove('dashboard-highlight');
+walletIcon.classList.add('wallet-highlight')
 }
